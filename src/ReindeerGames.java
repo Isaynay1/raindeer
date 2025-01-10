fe60d078961ab70f54e972f217fbcac1fa30529b
Isaynay1 <daviste1@midlandps.org>
2025-01-10 13:20:50 -0500

    Update ReindeerGames.java

--- src/ReindeerGames.java
+++ src/ReindeerGames.java
@@ -3,6 +3,24 @@ public class ReindeerGames {
 
 	public static void main(String[] args) {
 		// Start Here
+		String nam = "";
+		Arraylist<String>  raindeer = new Arraylist<String>;
+		raindeer.add("Dasher");
+		raindeer.add("Dancer");
+		raindeer.add("prancer");
+		raindeer.add("Vixen");
+		raindeer.add("Comet");
+		raindeer.add("Cupid");
+		raindeer.add("Donner");
+		raindeer.add("Blitzen");
+		raindeer.add("Rudolph");
+		for (int i =0 ; i < raindeer.size() ; i++){
+			nam = ((String)raindeer.get(i));
+			if (nam.Lenght() % 2 == 0){
+				nam = nam + "*";
+				raindeer.set(i,nam);
+			}
+		}
 
 	}
 
